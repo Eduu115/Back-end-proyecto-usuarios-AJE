cae7757b5981dee5477ab008cb049ad4cd3b1be7
Edu <serranotrenado.e@gmail.com>
2025-11-11 19:48:39 +0100

    created user dto and tested usuario RC

--- src/main/java/aje/restcontroller/UsuarioRestController.java
+++ src/main/java/aje/restcontroller/UsuarioRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import aje.model.entities.Usuario;
+import aje.model.entities.UsuarioDtoRegister;
 import aje.model.service.UsuarioService;
 
 @RestController
@@ -21,25 +22,30 @@ public class UsuarioRestController {
 	@Autowired
 	private UsuarioService us;
 	
-	@GetMapping("/todos")
+	@GetMapping("/") // lo usamos para la pagina de gestion admin TODO: Proteger
 	public ResponseEntity<?> todos(){
 		return ResponseEntity.ok(us.findAll());
 	}
 	
-	@PostMapping("/login")
-	 public ResponseEntity<?> login(@AuthenticationPrincipal Usuario usuario) {
-	        // Si llega aquí, ya está autenticado por Spring Security (HTTP Basic)
-	        us.findByUsernamePassword(usuario.getUsername(), usuario.getPassword());
-	        return ResponseEntity.ok().body(us.findByUsernamePassword(usuario.getUsername(), usuario.getPassword()));
-	    }
+	@GetMapping("/login")
+	public ResponseEntity<?> login(@AuthenticationPrincipal Usuario usuario) {
+	    // Si llega aquí, ya está autenticado por Spring Security (HTTP Basic)
+		Usuario u = us.findByUsernamePassword(usuario.getUsername(), usuario.getPassword());
+	    return ResponseEntity.ok().body(u);
+	}
 	
 	@PostMapping("/registro")
-	 public ResponseEntity<?> registro(@RequestBody Usuario usuario) {
-	        // Si llega aquí, ya está autenticado por Spring Security (HTTP Basic)
-			us.registrar(usuario);
-	        return ResponseEntity.ok().body(usuario);
-	    }
-
+	public ResponseEntity<?> registro(@RequestBody UsuarioDtoRegister usuario) {
+	    // Si llega aquí, ya está autenticado por Spring Security (HTTP Basic)
+		Usuario ufinal = us.toUsuario(usuario);
+		us.registrar(ufinal);
+		return ResponseEntity.ok().body(ufinal);
+	}
 	
+	@PostMapping("/registro/admin")
+	public ResponseEntity<?> registroAdmin(@RequestBody Usuario usuario){ // aqui no pasa nada por que nos lo dan con todos los datos
+		us.registrar(usuario);
+		return ResponseEntity.ok().body(usuario);
+	}
 
 }
