00a8bd1b16b389550de2dea2e9107f423df29373
Eduu115 <serranotrenado.e@gmail.com>
2025-11-19 09:13:24 +0100

    fixed sql

---
+++ sql/scriptdb.sql
@@ -0,0 +1,24 @@
+drop database if exists usuarios_perfiles;
+
+create database usuarios_perfiles;
+
+USE usuarios_perfiles;
+
+create table perfiles(
+id_perfil int auto_increment PRIMARY KEY not null,
+nombre varchar(40) check (nombre in ('ROLE_ADMIN', 'ROLE_JEFE', 'ROLE_TRABAJADOR', 'ROLE_CLIENTE')) not null
+);
+
+create table usuarios (
+username varchar(50) PRIMARY KEY not null,
+nombre varchar(50) not null,
+apellidos varchar(100),
+email varchar(100) unique not null,
+password varchar(255) not null, -- check (password = '{noop}%%'),
+enabled int check (enabled in (1,0)) not null default 1, -- añadir al edit en la lsita de usuarios en la vista
+direccion varchar(150),
+fecha_nacimiento date,
+fecha_registro date,
+id_perfil int default 4, -- perfil id 4 = cliente
+FOREIGN KEY (id_perfil) references perfiles(id_perfil)
+);
